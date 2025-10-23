d709e4fa979376266f556203fee2657064e0cb95
pranav <pranavsinghbora@gmail.com>
2025-10-23 22:05:28 +0530

    Hashset using array

---
+++ src/Array/easy/DesignHashSet.java
@@ -0,0 +1,38 @@
+package Array.easy;
+
+class MyHashSet{
+
+    boolean[] set; // for each integer each index , since set stores only unique values
+
+    public MyHashSet() {
+        set = new boolean[1000001];
+    }
+
+    public void add(int key) {
+        set[key] = true;
+    }
+
+    public void remove(int key) {
+        set[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return set[key];
+    }
+
+}
+public class DesignHashSet {
+
+    public static void main(String[] args) {
+
+        MyHashSet myHashSet = new MyHashSet();
+        myHashSet.add(1);      // set = [1]
+        myHashSet.add(2);      // set = [1, 2]
+        System.out.println(myHashSet.contains(1)); // return True
+        System.out.println( myHashSet.contains(3)); // return False, (not found)
+        myHashSet.add(2);      // set = [1, 2]
+        System.out.println( myHashSet.contains(2)); // return True
+        myHashSet.remove(2);   // set = [1]
+        System.out.println(myHashSet.contains(2)); // return False, (already removed)
+    }
+}
